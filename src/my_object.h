1774495234ac7860a8b23b93a4db1665843b4745
yanwenbin <yanwenbin@nagain.com>
2021-09-05 08:02:44 +0800

    去掉object的无const函数

--- src/my_object.h
+++ src/my_object.h
@@ -12,12 +12,8 @@ public:
   Object() : id_(uuid()) {}
   virtual ~Object() {}
 
-  std::string id() { return id_; }
   std::string id() const { return id_; }
-
-  virtual const int class_type() { return kMyObjectId; }
   virtual const int class_type() const { return kMyObjectId; }
-  virtual const char* class_name() { return "object"; }
   virtual const char* class_name() const { return "object"; }
 
 protected:
