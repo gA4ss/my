275af225433443110013342716c79cb031b610ef
yanwenbin <yanwenbin@nagain.com>
2022-05-08 18:57:22 +0800

    添加doxygen mainpage

---
+++ src/my.h
@@ -0,0 +1,18 @@
+/**
+  * @file        my.h
+  * @author      yanwenbin@nagain.com
+  * @version     1.0.0
+  * @date        08-MAY-2022
+  * @brief       Doxygen mainpage
+  * @attention   
+  *
+  *
+  * @htmlonly
+  * <span style="font-weight: bold">History</span>
+  * @endhtmlonly
+  * Version|Auther|Date|Describe
+  * -------|------|----|--------
+  * 1.0.0|yanwenbin|08-MAY-2022|create
+  * 
+  * <h2><center>&copy;COPYRIGHT 2022 YanWenBin All Rights Reserved.</center></h2>
+  */
